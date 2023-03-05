af261b98250345a1969966906039f82b89cb3d75
rodolfoperez <rodolfoperez@tabasco.gob.mx>
2023-03-04 19:58:03 -0600

    se modifico los comentarios del codigo

--- Player.gd
+++ Player.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
-export var speed = 400
-var screen_size
+export var speed = 400 #Qué tan rápido se moverá el jugador (píxeles/seg). se exporta como una caracteristica de panel de herramientas
+var screen_size # Tamaño de la ventana del juego$AnimatedSprite 
 
-func _ready():
+
+func _ready(): #funciona principal o metodo principal
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
 	
 	# Movimiento del personaje (teclas)
-	var velocity = Vector2.ZERO
+	var velocity = Vector2.ZERO #El vector de movimiento del jugador
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -19,7 +20,7 @@ func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 		
-		# Permite el desplazamiento en pantalla del personaje
+		# Normaliza la velocidad del personaje de forma diagonal
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -28,29 +29,9 @@ func _process(delta):
 		
 	
 	# Esta parte delimita el alcance del personaje al tamaño del cuadro
+	# Aplicar clamp es restringir un valor a un determinado rango
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
-	# Cambia la posición del personaje
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip_v = velocity.y > 0
-		
-	"""
-	if velocity.x < 0: 
-		$AnimatedSprite.flip_h = true
-	else:
-		$AnimatedSprite.flip_h = false
-	"""
-	
-	
-func _on_Player_body_entered(body):
-	hide() # Player disappears after being hit.
-	emit_signal("hit")
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
+	#
